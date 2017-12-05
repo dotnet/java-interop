358d8e72de85001fceb658fe86180cb4e235770a
Atsushi Eno <atsushieno@gmail.com>
2017-12-05 11:38:39 +0900

    Reduce Android.* dependencies from this not-only-about-Android repo.

--- src/Java.Interop.NamingCustomAttributes/Android.App/InstrumentationAttribute.cs
+++ src/Java.Interop.NamingCustomAttributes/Android.App/InstrumentationAttribute.cs
@@ -1,7 +1,7 @@
 using System;
 
 namespace Android.App {
-	sealed partial class InstrumentationAttribute : Attribute {
+	sealed partial class InstrumentationAttribute : Attribute, Java.Interop.IJniNameProviderAttribute {
 		public string Name { get; set; }
 	}
 }
