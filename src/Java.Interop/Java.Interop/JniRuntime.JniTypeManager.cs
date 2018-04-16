dotnet/java-interop

commit 3f96359469b09602dc69bfa4b658a27c9128f243

[Java.Interop] Fix TryRegisterNativeMembers() return value (#295)
`JniRuntime+JniTypeManager::TryRegisterNativeMembers()`always
returned true. It should return true only when a register method is
found and called.

Only call `nativeClass.RegisterNativeMethods()` only when
`registrations` list is not empty. This removes a warning on Android:

	JNI RegisterNativeMethods: attempt to register 0 native methods for somepackage.SomeType

src/Java.Interop/Java.Interop/JniRuntime.JniTypeManager.cs
@@ -238,6 +238,7 @@ namespace Java.Interop {
 			static bool TryRegisterNativeMembers (JniType nativeClass, Type marshalType, string methods, MethodInfo registerMethod = null)
 			{
 				bool lockTaken = false;
+				bool rv = false;
 
 				try {
 					Monitor.TryEnter (sharedRegistrations, ref lockTaken);
@@ -249,22 +250,27 @@ namespace Java.Interop {
 						registrations = new List<JniNativeMethodRegistration> ();
 					}
 					JniNativeMethodRegistrationArguments arguments = new JniNativeMethodRegistrationArguments (registrations, methods);
-					if (registerMethod != null)
+					if (registerMethod != null) {
 						registerMethod.Invoke (null, new object [] { arguments });
-					else
-						FindAndCallRegisterMethod (marshalType, arguments);
-					nativeClass.RegisterNativeMethods (registrations.ToArray ());
+						rv = true;
+					} else
+						rv = FindAndCallRegisterMethod (marshalType, arguments);
+
+					if (registrations.Count > 0)
+						nativeClass.RegisterNativeMethods (registrations.ToArray ());
 				} finally {
 					if (lockTaken) {
 						Monitor.Exit (sharedRegistrations);
 					}
 				}
 
-				return true;
+				return rv;
 			}
 
-			static void FindAndCallRegisterMethod (Type marshalType, JniNativeMethodRegistrationArguments arguments)
+			static bool FindAndCallRegisterMethod (Type marshalType, JniNativeMethodRegistrationArguments arguments)
 			{
+				bool found = false;
+
 				foreach (var methodInfo in marshalType.GetRuntimeMethods ()) {
 					if (methodInfo.GetCustomAttribute (typeof (JniAddNativeMethodRegistrationAttribute)) == null) {
 						continue;
@@ -276,7 +282,11 @@ namespace Java.Interop {
 
 					var register = (Action<JniNativeMethodRegistrationArguments>)methodInfo.CreateDelegate (typeof (Action<JniNativeMethodRegistrationArguments>));
 					register (arguments);
+
+					found = true;
 				}
+
+				return found;
 			}
 		}
 	}

commit 60235842b4905ae90475cb6b219ced17e87c3207

[Java.Interop] Do not throw in RegisterNativeMembers (#297)
Commit 3f963594 inadvertently broke the world: when running the
[`Java.Interop-Tess.dll` unit tests we SIGSEGV][0]:

[0]: https://jenkins.mono-project.com/view/Xamarin.Android/job/Java.Interop/223/console

	Executing: mono --debug packages/NUnit.ConsoleRunner.3.7.0/tools/nunit3-console.exe  bin/TestRelease/Java.Interop-Tests.dll  --result="TestResult-Java.Interop-Tests.xml;format=nunit2" --output="bin/TestRelease/TestOutput-Java.Interop-Tests.txt"
	...
	CallNonvirtualDerived.method() invoked!
	Stacktrace:
	  at <unknown> <0xffffffff>
	  at (wrapper managed-to-native) Java.Interop.NativeMethods.java_interop_jnienv_get_object_class (intptr,intptr)
	  at Java.Interop.JniEnvironment/Types.GetObjectClass (Java.Interop.JniObjectReference)
	  at Java.Interop.JniEnvironment/Types.GetJniTypeNameFromInstance (Java.Interop.JniObjectReference)
	  at Java.Interop.JniRuntime/JniValueManager.DisposePeer (Java.Interop.JniObjectReference,Java.Interop.IJavaPeerable)
	  at Java.Interop.JniRuntime/JniValueManager.DisposePeer (Java.Interop.IJavaPeerable)
	  at Java.Interop.JavaObject.Dispose ()
	  at Java.InteropTests.TestTypeTests.PropogateException ()
	  at (wrapper runtime-invoke) object.runtime_invoke_void__this__ (object,intptr,intptr,intptr)
	  ...
	Native stacktrace:
		0   mono                                0x000000010b58b251 mono_handle_native_crash + 257
		1   mono                                0x000000010b6279f6 altstack_handle_and_restore + 70
		2   libjvm.dylib                        0x00000001134bdab6 _ZN17HandleMarkCleanerD2Ev + 22
		3   libjvm.dylib                        0x00000001134ac6af jni_GetObjectClass + 179
		4   libjvm.dylib                        0x00000001134c0c11 checked_jni_GetObjectClass + 239
		5   libjava-interop.dylib               0x0000000129b55ad3 java_interop_jnienv_get_object_class + 51

src/Java.Interop/Java.Interop/JniRuntime.JniTypeManager.cs
@@ -212,12 +212,14 @@ namespace Java.Interop {
 
 			public virtual void RegisterNativeMembers (JniType nativeClass, Type type, string methods)
 			{
+				TryRegisterNativeMembers (nativeClass, type, methods);
+			}
+
+			protected bool TryRegisterNativeMembers (JniType nativeClass, Type type, string methods)
+			{
 				AssertValid ();
 
-				if (!TryLoadJniMarshalMethods (nativeClass, type, methods) &&
-						!TryRegisterNativeMembers (nativeClass, type, methods)) {
-					throw new NotSupportedException ($"Could not register Java.class={nativeClass.Name} Managed.type={type.FullName}");
-				}
+				return TryLoadJniMarshalMethods (nativeClass, type, methods) || TryRegisterNativeMembers (nativeClass, type, methods, null);
 			}
 
 			static Type [] registerMethodParameters = new Type [] { typeof (JniNativeMethodRegistrationArguments) };
@@ -235,7 +237,7 @@ namespace Java.Interop {
 
 			static List<JniNativeMethodRegistration> sharedRegistrations = new List<JniNativeMethodRegistration> ();
 
-			static bool TryRegisterNativeMembers (JniType nativeClass, Type marshalType, string methods, MethodInfo registerMethod = null)
+			static bool TryRegisterNativeMembers (JniType nativeClass, Type marshalType, string methods, MethodInfo registerMethod)
 			{
 				bool lockTaken = false;
 				bool rv = false;

src/Java.Interop/java/com/xamarin/java_interop/internal/JavaProxyThrowable.java
@@ -11,7 +11,7 @@ import com.xamarin.java_interop.GCUserPeerable;
 	static  final   String  assemblyQualifiedName   = "Java.Interop.JavaProxyThrowable, Java.Interop, Version=0.1.0.0, Culture=neutral, PublicKeyToken=null";
 	static {
 		com.xamarin.java_interop.ManagedPeer.registerNativeMembers (
-				JavaProxyObject.class,
+				JavaProxyThrowable.class,
 				assemblyQualifiedName,
 				"");
 	}