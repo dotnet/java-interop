8de531305506df5c39218de0fe94310de37c8627
Atsushi Eno <atsushieno@gmail.com>
2018-07-02 18:11:43 +0900

    Add a bit more complicated DIM tests: property setter, String, String[], params.

--- tools/generator/Tests/expected.ji/DefaultInterfaceMethods/Xamarin.Test.ITheInterface.cs
+++ tools/generator/Tests/expected.ji/DefaultInterfaceMethods/Xamarin.Test.ITheInterface.cs
@@ -43,6 +43,65 @@ namespace Xamarin.Test {
 			}
 		}
 
+		static Delegate cb_getStringProp;
+#pragma warning disable 0169
+		static Delegate GetGetStringPropHandler ()
+		{
+			if (cb_getStringProp == null)
+				cb_getStringProp = JNINativeWrapper.CreateDelegate ((Func<IntPtr, IntPtr, IntPtr>) n_GetStringProp);
+			return cb_getStringProp;
+		}
+
+		static IntPtr n_GetStringProp (IntPtr jnienv, IntPtr native__this)
+		{
+			global::Xamarin.Test.ITheInterface __this = global::Java.Lang.Object.GetObject<global::Xamarin.Test.ITheInterface> (jnienv, native__this, JniHandleOwnership.DoNotTransfer);
+			return JNIEnv.NewString (__this.StringProp);
+		}
+#pragma warning restore 0169
+
+		static Delegate cb_setStringProp_Ljava_lang_String_;
+#pragma warning disable 0169
+		static Delegate GetSetStringProp_Ljava_lang_String_Handler ()
+		{
+			if (cb_setStringProp_Ljava_lang_String_ == null)
+				cb_setStringProp_Ljava_lang_String_ = JNINativeWrapper.CreateDelegate ((Action<IntPtr, IntPtr, IntPtr>) n_SetStringProp_Ljava_lang_String_);
+			return cb_setStringProp_Ljava_lang_String_;
+		}
+
+		static void n_SetStringProp_Ljava_lang_String_ (IntPtr jnienv, IntPtr native__this, IntPtr native_v)
+		{
+			global::Xamarin.Test.ITheInterface __this = global::Java.Lang.Object.GetObject<global::Xamarin.Test.ITheInterface> (jnienv, native__this, JniHandleOwnership.DoNotTransfer);
+			string v = JNIEnv.GetString (native_v, JniHandleOwnership.DoNotTransfer);
+			__this.StringProp = v;
+		}
+#pragma warning restore 0169
+
+		 virtual unsafe string StringProp {
+			// Metadata.xml XPath method reference: path="/api/package[@name='xamarin.test']/interface[@name='TheInterface']/method[@name='getStringProp' and count(parameter)=0]"
+			[Register ("getStringProp", "()Ljava/lang/String;", "GetGetStringPropHandler")]
+			get {
+				const string __id = "getStringProp.()Ljava/lang/String;";
+				try {
+					var __rm = _members.InstanceMethods.InvokeVirtualObjectMethod (__id, (IJavaPeerable) this, null);
+					return JNIEnv.GetString (__rm.Handle, JniHandleOwnership.TransferLocalRef);
+				} finally {
+				}
+			}
+			// Metadata.xml XPath method reference: path="/api/package[@name='xamarin.test']/interface[@name='TheInterface']/method[@name='setStringProp' and count(parameter)=1 and parameter[1][@type='java.lang.String']]"
+			[Register ("setStringProp", "(Ljava/lang/String;)V", "GetSetStringProp_Ljava_lang_String_Handler")]
+			set {
+				const string __id = "setStringProp.(Ljava/lang/String;)V";
+				IntPtr native_value = JNIEnv.NewString (value);
+				try {
+					JniArgumentValue* __args = stackalloc JniArgumentValue [1];
+					__args [0] = new JniArgumentValue (native_value);
+					_members.InstanceMethods.InvokeVirtualVoidMethod (__id, (IJavaPeerable) this, __args);
+				} finally {
+					JNIEnv.DeleteLocalRef (native_value);
+				}
+			}
+		}
+
 		static Delegate cb_foo;
 #pragma warning disable 0169
 		static Delegate GetFooHandler ()
@@ -68,6 +127,86 @@ namespace Xamarin.Test {
 				var __rm = _members.InstanceMethods.InvokeVirtualInt32Method (__id, (IJavaPeerable) this, null);
 				return __rm;
 			} finally {
+			}
+		}
+
+		static Delegate cb_string1;
+#pragma warning disable 0169
+		static Delegate GetString1Handler ()
+		{
+			if (cb_string1 == null)
+				cb_string1 = JNINativeWrapper.CreateDelegate ((Func<IntPtr, IntPtr, IntPtr>) n_String1);
+			return cb_string1;
+		}
+
+		static IntPtr n_String1 (IntPtr jnienv, IntPtr native__this)
+		{
+			global::Xamarin.Test.ITheInterface __this = global::Java.Lang.Object.GetObject<global::Xamarin.Test.ITheInterface> (jnienv, native__this, JniHandleOwnership.DoNotTransfer);
+			return JNIEnv.NewString (__this.String1 ());
+		}
+#pragma warning restore 0169
+
+		// Metadata.xml XPath method reference: path="/api/package[@name='xamarin.test']/interface[@name='TheInterface']/method[@name='string1' and count(parameter)=0]"
+		[Register ("string1", "()Ljava/lang/String;", "GetString1Handler")]
+		 virtual unsafe string String1 ()
+		{
+			const string __id = "string1.()Ljava/lang/String;";
+			try {
+				var __rm = _members.InstanceMethods.InvokeVirtualObjectMethod (__id, (IJavaPeerable) this, null);
+				return JNIEnv.GetString (__rm.Handle, JniHandleOwnership.TransferLocalRef);
+			} finally {
+			}
+		}
+
+		static Delegate cb_string1_Ljava_lang_String_arrayLjava_lang_String_arrayLjava_lang_String_;
+#pragma warning disable 0169
+		static Delegate GetString1_Ljava_lang_String_arrayLjava_lang_String_arrayLjava_lang_String_Handler ()
+		{
+			if (cb_string1_Ljava_lang_String_arrayLjava_lang_String_arrayLjava_lang_String_ == null)
+				cb_string1_Ljava_lang_String_arrayLjava_lang_String_arrayLjava_lang_String_ = JNINativeWrapper.CreateDelegate ((Func<IntPtr, IntPtr, IntPtr, IntPtr, IntPtr, IntPtr>) n_String1_Ljava_lang_String_arrayLjava_lang_String_arrayLjava_lang_String_);
+			return cb_string1_Ljava_lang_String_arrayLjava_lang_String_arrayLjava_lang_String_;
+		}
+
+		static IntPtr n_String1_Ljava_lang_String_arrayLjava_lang_String_arrayLjava_lang_String_ (IntPtr jnienv, IntPtr native__this, IntPtr native_p1, IntPtr native_p2, IntPtr native_p3)
+		{
+			global::Xamarin.Test.ITheInterface __this = global::Java.Lang.Object.GetObject<global::Xamarin.Test.ITheInterface> (jnienv, native__this, JniHandleOwnership.DoNotTransfer);
+			string p1 = JNIEnv.GetString (native_p1, JniHandleOwnership.DoNotTransfer);
+			string[] p2 = (string[]) JNIEnv.GetArray (native_p2, JniHandleOwnership.DoNotTransfer, typeof (string));
+			string[] p3 = (string[]) JNIEnv.GetArray (native_p3, JniHandleOwnership.DoNotTransfer, typeof (string));
+			IntPtr __ret = JNIEnv.NewString (__this.String1 (p1, p2, p3));
+			if (p2 != null)
+				JNIEnv.CopyArray (p2, native_p2);
+			if (p3 != null)
+				JNIEnv.CopyArray (p3, native_p3);
+			return __ret;
+		}
+#pragma warning restore 0169
+
+		// Metadata.xml XPath method reference: path="/api/package[@name='xamarin.test']/interface[@name='TheInterface']/method[@name='string1' and count(parameter)=3 and parameter[1][@type='java.lang.String'] and parameter[2][@type='java.lang.String[]'] and parameter[3][@type='java.lang.String...']]"
+		[Register ("string1", "(Ljava/lang/String;[Ljava/lang/String;[Ljava/lang/String;)Ljava/lang/String;", "GetString1_Ljava_lang_String_arrayLjava_lang_String_arrayLjava_lang_String_Handler")]
+		 virtual unsafe string String1 (string p1, string[] p2, params string[] p3)
+		{
+			const string __id = "string1.(Ljava/lang/String;[Ljava/lang/String;[Ljava/lang/String;)Ljava/lang/String;";
+			IntPtr native_p1 = JNIEnv.NewString (p1);
+			IntPtr native_p2 = JNIEnv.NewArray (p2);
+			IntPtr native_p3 = JNIEnv.NewArray (p3);
+			try {
+				JniArgumentValue* __args = stackalloc JniArgumentValue [3];
+				__args [0] = new JniArgumentValue (native_p1);
+				__args [1] = new JniArgumentValue (native_p2);
+				__args [2] = new JniArgumentValue (native_p3);
+				var __rm = _members.InstanceMethods.InvokeVirtualObjectMethod (__id, (IJavaPeerable) this, __args);
+				return JNIEnv.GetString (__rm.Handle, JniHandleOwnership.TransferLocalRef);
+			} finally {
+				JNIEnv.DeleteLocalRef (native_p1);
+				if (p2 != null) {
+					JNIEnv.CopyArray (native_p2, p2);
+					JNIEnv.DeleteLocalRef (native_p2);
+				}
+				if (p3 != null) {
+					JNIEnv.CopyArray (native_p3, p3);
+					JNIEnv.DeleteLocalRef (native_p3);
+				}
 			}
 		}
 
