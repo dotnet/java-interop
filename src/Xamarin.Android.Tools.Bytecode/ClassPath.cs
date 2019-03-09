36629f5fe94f63a2c33e14d52bcf6f6f33f3a21e
Matthew Leibowitz <mattleibow@live.com>
2019-03-09 20:49:21 +0200

    Allow .jar files to be read from a Stream

--- src/Xamarin.Android.Tools.Bytecode/ClassPath.cs
+++ src/Xamarin.Android.Tools.Bytecode/ClassPath.cs
@@ -46,7 +46,17 @@ namespace Xamarin.Android.Tools.Bytecode {
 			if (!IsJarFile (jarFile))
 				throw new ArgumentException ("'jarFile' is not a valid .jar file.", "jarFile");
 
-			using (var jar = CreateZipArchive (jarFile)) {
+			using (var jarStream = File.OpenRead (jarFile)) { 
+				Load (jarStream);
+			}
+		}
+
+		public void Load (Stream jarStream)
+		{
+			if (jarStream == null)
+				throw new ArgumentNullException (nameof (jarStream));
+
+			using (var jar = CreateZipArchive (jarStream)) {
 				foreach (var entry in jar.Entries) {
 					if (entry.Length == 0)
 						continue;
@@ -67,11 +77,11 @@ namespace Xamarin.Android.Tools.Bytecode {
 			}
 		}
 
-		static ZipArchive CreateZipArchive (string jarFile)
+		static ZipArchive CreateZipArchive (Stream jarStream)
 		{
 			var encoding    = new UTF8Encoding (encoderShouldEmitUTF8Identifier: false);
 
-			return new ZipArchive (File.OpenRead (jarFile), ZipArchiveMode.Read, leaveOpen: false, entryNameEncoding: encoding);
+			return new ZipArchive (jarStream, ZipArchiveMode.Read, leaveOpen: true, entryNameEncoding: encoding);
 		}
 
 		public void Add (ClassFile classFile)
