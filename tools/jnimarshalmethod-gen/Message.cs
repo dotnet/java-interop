c5411a28e731a762305b4051030d4933d51335f1
Radek Doulik <radekdoulik@gmail.com>
2020-08-25 14:53:00 +0200

    Remove clutter

--- tools/jnimarshalmethod-gen/Message.cs
+++ tools/jnimarshalmethod-gen/Message.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Java.Interop.Localization;
 
 namespace Xamarin.Android.Tools.JniMarshalMethodGenerator
