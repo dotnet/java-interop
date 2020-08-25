7b216eb648a8e8d5d5902ae0042ca9743a21504f
Radek Doulik <radek.doulik@gmail.com>
2020-08-25 20:17:51 +0200

    Add missing `using System.Runtime.InteropServices`

--- src/Java.Interop/Properties/AssemblyInfo.cs
+++ src/Java.Interop/Properties/AssemblyInfo.cs
@@ -1,5 +1,6 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 [assembly: DefaultDllImportSearchPathsAttribute (DllImportSearchPath.SafeDirectories | DllImportSearchPath.AssemblyDirectory)]
 
