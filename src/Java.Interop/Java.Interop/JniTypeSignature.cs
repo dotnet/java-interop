670acfd829a038b2b569e3ebe56781ef4daf00c6
Radek Doulik <radekdoulik@gmail.com>
2020-11-23 12:54:18 +0100

    Keep NETCOREAPP in place
    ...until the situation around NET defines clears out.
    
    Contex: https://github.com/dotnet/designs/pull/165

--- src/Java.Interop/Java.Interop/JniTypeSignature.cs
+++ src/Java.Interop/Java.Interop/JniTypeSignature.cs
@@ -172,7 +172,7 @@ namespace Java.Interop
 
 		public override int GetHashCode ()
 		{
-#if NET5_0
+#if NETCOREAPP
 			return QualifiedReference.GetHashCode (StringComparison.Ordinal);
 #else
 			return QualifiedReference.GetHashCode ();
