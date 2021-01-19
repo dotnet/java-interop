47c20cfc2a3f4c3df88bf57528a73465fadecbce
Radek Doulik <radekdoulik@gmail.com>
2021-01-19 23:15:47 +0100

    Add ActivatePeer to MyValueManager

--- tests/Java.Interop-Tests/Java.Interop/JniRuntime.JniValueManagerTests.cs
+++ tests/Java.Interop-Tests/Java.Interop/JniRuntime.JniValueManagerTests.cs
@@ -59,6 +59,11 @@ namespace Java.InteropTests {
 			{
 				return null;
 			}
+
+			public override void ActivatePeer (JniObjectReference reference, Type peerType, Type [] constructorArguments, object [] argumentValues)
+			{
+				throw new NotImplementedException ();
+			}
 		}
 
 		[Test]
