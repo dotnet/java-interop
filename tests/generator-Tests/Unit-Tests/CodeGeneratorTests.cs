c6943139c43192a735e70ead9a72adb06b35fba8
Jonathan Pobst <jonathan.pobst@microsoft.com>
2023-02-28 10:54:34 -1000

    [generator] Allow 'managedOverride' metadata to support 'abstract' methods.

--- tests/generator-Tests/Unit-Tests/CodeGeneratorTests.cs
+++ tests/generator-Tests/Unit-Tests/CodeGeneratorTests.cs
@@ -151,6 +151,30 @@ namespace generatortests
 		}
 
 		[Test]
+		public void ManagedOverrideAbstractMethod_Override ()
+		{
+			var xml = @"<api>
+			  <package name='java.lang' jni-name='java/lang'>
+			    <class abstract='false' deprecated='not deprecated' final='false' name='Object' static='false' visibility='public' jni-signature='Ljava/lang/Object;' />
+			  </package>
+			  <package name='com.xamarin.android' jni-name='com/xamarin/android'>
+			    <class abstract='false' deprecated='not deprecated' extends='java.lang.Object' extends-generic-aware='java.lang.Object' jni-extends='Ljava/lang/Object;' final='false' name='MyClass' static='false' visibility='public' jni-signature='Lcom/xamarin/android/MyClass;'>
+			      <method abstract='true' deprecated='not deprecated' final='true' name='DoStuff' jni-signature='()I' bridge='false' native='false' return='int' jni-return='I' static='false' synchronized='false' synthetic='false' visibility='public' managedOverride='override'></method>
+			    </class>
+			  </package>
+			</api>";
+
+			var gens = ParseApiDefinition (xml);
+			var klass = gens.Single (g => g.Name == "MyClass");
+
+			generator.Context.ContextTypes.Push (klass);
+			generator.WriteType (klass, string.Empty, new GenerationInfo ("", "", "MyAssembly"));
+			generator.Context.ContextTypes.Pop ();
+
+			Assert.True (writer.ToString ().Contains ("public override abstract int DoStuff ();"), $"was: `{writer}`");
+		}
+
+		[Test]
 		public void ManagedOverrideMethod_None ()
 		{
 			var xml = @"<api>
