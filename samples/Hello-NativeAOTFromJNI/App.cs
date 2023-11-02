1fc0af5213af94e910dec7b1d2ac2ee8ab6e1ab5
Jonathan Pryor <jonpryor@vt.edu>
2023-11-02 11:03:07 -0400

    Use JniTransition.

--- samples/Hello-NativeAOTFromJNI/App.cs
+++ samples/Hello-NativeAOTFromJNI/App.cs
@@ -6,15 +6,26 @@ namespace Hello_NativeAOTFromJNI;
 
 static class App {
 
-	// symbol name from `$(IntermediateOutputPath)/h-classes/com_microsoft_hello_from_jni_NativeAOTInit.h`
+	// symbol name from `$(IntermediateOutputPath)h-classes/com_microsoft_hello_from_jni_App.h`
 	[UnmanagedCallersOnly (EntryPoint="Java_com_microsoft_hello_1from_1jni_App_sayHello")]
 	static IntPtr sayHello (IntPtr jnienv, IntPtr klass)
 	{
-		var s = $"Hello from .NET NativeAOT!";
-		Console.WriteLine (s);
-		var h = JniEnvironment.Strings.NewString (s);
-		var r = JniEnvironment.References.NewReturnToJniRef (h);
-		JniObjectReference.Dispose (ref h);
-		return r;
+		var envp = new JniTransition (jnienv);
+		try {
+			var s = $"Hello from .NET NativeAOT!";
+			Console.WriteLine (s);
+			var h = JniEnvironment.Strings.NewString (s);
+			var r = JniEnvironment.References.NewReturnToJniRef (h);
+			JniObjectReference.Dispose (ref h);
+			return r;
+		}
+		catch (Exception e) {
+			Console.Error.WriteLine ($"Error in App.sayHello(): {e.ToString ()}");
+			envp.SetPendingException (e);
+		}
+		finally {
+			envp.Dispose ();
+		}
+		return nint.Zero;
 	}
 }
