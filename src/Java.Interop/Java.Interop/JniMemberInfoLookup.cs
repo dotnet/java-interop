41254f23ffee57f94f53686a3a1b3f1a1d6802f7
Jonathan Pryor (HE/HIM) <jopryo@microsoft.com>
2024-03-29 18:58:54 -0400

    Fix PublicAPI.Unshipped.txt errors.

--- src/Java.Interop/Java.Interop/JniMemberInfoLookup.cs
+++ src/Java.Interop/Java.Interop/JniMemberInfoLookup.cs
@@ -7,6 +7,12 @@ public ref struct JniMemberInfoLookup {
 	public  ReadOnlySpan<byte>      MemberName      {get; private set;}
 	public  ReadOnlySpan<byte>      MemberSignature {get; private set;}
 
+	[Obsolete ("Use the JniMemberInfoLookup(string, ReadOnlySpan<byte>, ReadOnlySpan<byte>) constructor.", error: true)]
+	public JniMemberInfoLookup ()
+	{
+		throw new NotSupportedException ();
+	}
+
 	public JniMemberInfoLookup (string encodedMember, ReadOnlySpan<byte> memberName, ReadOnlySpan<byte> memberSignature)
 	{
 		EncodedMember   = encodedMember;
