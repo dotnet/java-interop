27236f2b830d7d4b6883752fe10de17c1c0249b8
Jonathan Pryor <jonpryor@vt.edu>
2024-06-28 15:02:53 -0400

    [Java.Interop] Add `.JavaAs()` extension method
    Fixes: https://github.com/dotnet/android/issues/9038
    
    Context: 1adb7964a2033c83c298c070f2d1ab896d92671b
    
    Imagine the following Java type hierarchy:
    
    	// Java
    	public abstract class Drawable {
    	    public static Drawable createFromStream(IntputStream is, String srcName) {…}
    	    // …
    	}
    	public interface Animatable {
    	    public void start();
    	    // …
    	}
    	/* package */ class SomeAnimatableDrawable extends Drawable implements Animatable {
    	    // …
    	}
    
    Further imagine that a call to `Drawable.createFromStream()` returns
    an instance of `SomeAnimatableDrawable`.
    
    What does the *binding* `Drawable.CreateFromStream()` return?
    
    	// C#
    	var drawable = Drawable.CreateFromStream(input, name);
    
    The binding `Drawable.CreateFromStream()` look at the runtime type
    of the value returned, see that it's of type `SomeAnimatableDrawable`,
    and look for an existing binding of that type.  If no such binding is
    found -- which will be the case here, as `SomeAnimatableDrawable` is
    package-private -- then we check the value's base class, ad infinitum,
    until we hit a type that we *do* have a binding for (or fail
    catastrophically when we can't find a binding for `java.lang.Object`).
    See also [`TypeManager.CreateInstance()`][0], which is similar to the
    code within `JniRuntime.JniValueManager.GetPeerConstructor()`.
    
    Any interfaces implemented by Java value are not consulted.  Only
    the base class hiearchy.
    
    For the sake of discussion, assume that `drawable` will be an
    instance of `DrawableInvoker` (e.g. 1adb7964), akin to:
    
    	internal class DrawableInvoker : Drawable {
    	    // …
    	}
    
    Further imagine that we want to invoke `Animatable` methods on
    `drawable`.  How do we do this?
    
    This is where the [`.JavaCast<TResult>()` extension method][1] comes
    in: we can use `.JavaCast<TResult>()` to perform a Java-side type
    check the type cast, which returns a value which can be used to
    invoke methods on the specified type:
    
    	var animatable = drawable.JavaCast<IAnimatable>();
    	animatable.Start();
    
    The problem with `.JavaCast<TResult>()` is that it always throws on
    failure:
    
    	var someOtherIface = drawable.JavaCast<ISomethingElse>();
    	// throws some exception…
    
    @mattleibow requests an "exception-free JavaCast overload" so that he
    can *easily* use type-specific functionality *optionally*.
    
    Add the following extension methods on `IJavaPeerable`:
    
    	static class JavaPeerableExtensions {
    	    public static TResult? JavaAs<TResult>(
    	            this IJavaPeerable self);
    	    public static bool TryJavaCast<TResult>(
    	            this IJavaPeerable self,
    	            out TResult? result);
    	}
    
    The `.JavaAs<TResult>()` extension method mirrors the C# `as`
    operator, returning `null` if the type coercion would fail.
    This makes it useful for one-off invocations:
    
    	drawable.JavaAs<IAnimatable>()?.Start();
    
    The `.TryJavaCast<TResult>()` extension method follows the
    `TryParse()` pattern, returning true if the type coercion succeeds
    and the output `result` parameter is non-null, and false otherwise.
    This allows "nicely scoping" things within an `if`:
    
    	if (drawable.TryJavaCast<IAnimatable>(out var animatable)) {
    	    animatable.Start();
    	    // …
    	    animatable.Stop();
    	}
    
    [0]: https://github.com/dotnet/android/blob/06bb1dc6a292ef5618a3bb6ecca3ca869253ff2e/src/Mono.Android/Java.Interop/TypeManager.cs#L276-L291
    [1]: https://github.com/dotnet/android/blob/06bb1dc6a292ef5618a3bb6ecca3ca869253ff2e/src/Mono.Android/Android.Runtime/Extensions.cs#L9-L17

--- src/Java.Interop/Java.Interop/JavaPeerableExtensions.cs
+++ src/Java.Interop/Java.Interop/JavaPeerableExtensions.cs
@@ -1,6 +1,7 @@
 #nullable enable
 
 using System;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Java.Interop {
 
@@ -10,6 +11,41 @@ namespace Java.Interop {
 		{
 			JniPeerMembers.AssertSelf (self);
 			return JniEnvironment.Types.GetJniTypeNameFromInstance (self.PeerReference);
+		}
+
+		public static bool TryJavaCast<
+				[DynamicallyAccessedMembers (JavaObject.ConstructorsAndInterfaces)]
+				TResult
+		> (this IJavaPeerable? self, [NotNullWhen (true)] out TResult? result)
+			where TResult : class, IJavaPeerable
+		{
+			result = JavaAs<TResult> (self);
+			return result != null;
+		}
+
+		public static TResult? JavaAs<
+				[DynamicallyAccessedMembers (JavaObject.ConstructorsAndInterfaces)]
+				TResult
+		> (this IJavaPeerable? self)
+			where TResult : class, IJavaPeerable
+		{
+			if (self == null) {
+				return null;
+			}
+
+			if (self is TResult result) {
+				return result;
+			}
+
+			if (!self.PeerReference.IsValid) {
+				throw new ObjectDisposedException (self.GetType ().FullName);
+			}
+
+			var r = self.PeerReference;
+			return JniEnvironment.Runtime.ValueManager.CreatePeer (
+					ref r, JniObjectReferenceOptions.Copy,
+					targetType: typeof (TResult))
+				as TResult;
 		}
 	}
 }
---
+++ tests/Java.Interop-Tests/java/net/dot/jni/test/MyJavaInterfaceImpl.java
@@ -0,0 +1,9 @@
+package net.dot.jni.test;
+
+public class MyJavaInterfaceImpl
+	implements JavaInterface
+{
+	public String getValue() {
+		return "Hello from Java!";
+	}
+}
