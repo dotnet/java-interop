5ac2e33420ceaa4f7838f62c9cfd1d3e033e16c9
Jonathan Pryor <jonpryor@vt.edu>
2024-06-29 09:21:29 -0400

    Also implement Cloneable so dotnet/android can test it.

--- tests/Java.Interop-Tests/java/net/dot/jni/test/MyJavaInterfaceImpl.java
+++ tests/Java.Interop-Tests/java/net/dot/jni/test/MyJavaInterfaceImpl.java
@@ -1,9 +1,13 @@
 package net.dot.jni.test;
 
 public class MyJavaInterfaceImpl
-	implements JavaInterface
+	implements JavaInterface, Cloneable
 {
 	public String getValue() {
 		return "Hello from Java!";
+	}
+
+	public Object clone() {
+		return this;
 	}
 }
