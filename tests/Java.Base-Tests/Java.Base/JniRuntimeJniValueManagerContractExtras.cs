9aed73747ccd1178aeaae40dc7da7e148a29eb18
Jonathan Pryor <jonpryor@vt.edu>
2025-03-27 11:57:35 -0400

    Fix formatting.
    TABS NOT SPACES

--- tests/Java.Base-Tests/Java.Base/JniRuntimeJniValueManagerContractExtras.cs
+++ tests/Java.Base-Tests/Java.Base/JniRuntimeJniValueManagerContractExtras.cs
@@ -12,16 +12,16 @@ public class JniRuntimeJniValueManagerContractExtras : JavaVMFixture {
 	[Test]
 	public void CreatePeer_FloatIsNotNullableSingle ()
 	{
-        JniObjectReference float_ref;
-        using (var value = new Java.Lang.Float (1.0f)) {
-            float_ref = value.PeerReference.NewLocalRef ();
-        }
-        try {
-            var peer = JniEnvironment.Runtime.ValueManager.CreatePeer (ref float_ref, JniObjectReferenceOptions.Copy, targetType: null);
-            Assert.IsNotNull (peer, "Could not create Java.Lang.Float peer!");
-            Assert.AreEqual (typeof (Java.Lang.Float), peer.GetType (), $"Peer type mismatch: expected Java.Lang.Float, got {peer.GetType ()}");
-        } finally {
-            JniObjectReference.Dispose (ref float_ref);
-        }
+		JniObjectReference float_ref;
+		using (var value = new Java.Lang.Float (1.0f)) {
+			float_ref   = value.PeerReference.NewLocalRef ();
+		}
+		try {
+			var peer    = JniEnvironment.Runtime.ValueManager.CreatePeer (ref float_ref, JniObjectReferenceOptions.Copy, targetType: null);
+			Assert.IsNotNull (peer, "Could not create Java.Lang.Float peer!");
+			Assert.AreEqual (typeof (Java.Lang.Float), peer.GetType (), $"Peer type mismatch: expected Java.Lang.Float, got {peer.GetType ()}");
+		} finally {
+			JniObjectReference.Dispose (ref float_ref);
+		}
 	}
 }
