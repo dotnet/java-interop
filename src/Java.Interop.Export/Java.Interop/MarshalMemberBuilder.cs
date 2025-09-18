f38856e4fbd805affbd48be3cac3aaf77eac4f5f
Simon Rozsival <simon@rozsival.com>
2025-09-18 21:46:30 +0200

    Revert change that is not necessary thanks to the introduction of the Marshaler property

--- src/Java.Interop.Export/Java.Interop/MarshalMemberBuilder.cs
+++ src/Java.Interop.Export/Java.Interop/MarshalMemberBuilder.cs
@@ -64,10 +64,11 @@ namespace Java.Interop {
 				throw new ArgumentNullException ("method");
 
 			string signature = GetJniMethodSignature (export, method);
-			return new JniNativeMethodRegistration (
-				GetJniMethodName (export, method),
-				signature,
-				CreateJniMethodMarshaler (method, export, type));
+			return new JniNativeMethodRegistration () {
+				Name        = GetJniMethodName (export, method),
+				Signature   = signature,
+				Marshaler   = CreateJniMethodMarshaler (method, export, type),
+			};
 		}
 
 		string GetJniMethodName (JavaCallableAttribute export, MethodInfo method)
