6d52b54fa73acbab89b10cc36abcb8cd5f48c7f4
Simon Rozsival <simon@rozsival.com>
2026-06-08 13:21:40 +0200

    Update NativeAOT sample

--- samples/Hello-NativeAOTFromJNI/NativeAotTypeManager.cs
+++ samples/Hello-NativeAOTFromJNI/NativeAotTypeManager.cs
@@ -3,8 +3,7 @@ using System.Diagnostics.CodeAnalysis;
 
 namespace Hello_NativeAOTFromJNI;
 
-class NativeAotTypeManager : JniRuntime.JniTypeManager {
-	internal const DynamicallyAccessedMemberTypes Constructors = DynamicallyAccessedMemberTypes.PublicConstructors | DynamicallyAccessedMemberTypes.NonPublicConstructors;
+class NativeAotTypeManager : JniRuntime.DynamicJniTypeManager {
 	internal const DynamicallyAccessedMemberTypes Methods = DynamicallyAccessedMemberTypes.PublicMethods | DynamicallyAccessedMemberTypes.NonPublicMethods;
 	internal const DynamicallyAccessedMemberTypes MethodsAndPrivateNested = Methods | DynamicallyAccessedMemberTypes.NonPublicNestedTypes;
 
@@ -16,32 +15,25 @@ class NativeAotTypeManager : JniRuntime.JniTypeManager {
 	};
 #pragma warning restore IL2026
 
-	protected override string? GetSimpleReference (Type type)
-	{
-		foreach (var e in typeMappings) {
-			if (e.Value == type)
-				return e.Key;
-		}
-		return null;
-	}
-
-
 	protected override IEnumerable<Type> GetTypesForSimpleReference (string jniSimpleReference)
 	{
 		if (typeMappings.TryGetValue (jniSimpleReference, out var target))
 			yield return target;
+		foreach (var type in base.GetTypesForSimpleReference (jniSimpleReference))
+			yield return type;
 	}
 
 	protected override Type? GetTypeForSimpleReference (string jniSimpleReference)
 	{
 		if (typeMappings.TryGetValue (jniSimpleReference, out var target))
 			return target;
-		return null;
+		return base.GetTypeForSimpleReference (jniSimpleReference);
 	}
 
 	protected override IEnumerable<string> GetSimpleReferences (Type type)
 	{
-		return CreateSimpleReferencesEnumerator (type);
+		return base.GetSimpleReferences (type)
+			.Concat (CreateSimpleReferencesEnumerator (type));
 	}
 
 	IEnumerable<string> CreateSimpleReferencesEnumerator (Type type)
@@ -53,41 +45,6 @@ class NativeAotTypeManager : JniRuntime.JniTypeManager {
 				yield return e.Key;
 		}
 	}
-
-	public override IEnumerable<Type> GetTypes (JniTypeSignature typeSignature)
-	{
-		if (!typeSignature.IsValid || typeSignature.ArrayRank != 0 || typeSignature.SimpleReference == null)
-			return [];
-		return GetTypesForSimpleReference (typeSignature.SimpleReference);
-	}
-
-	public override IEnumerable<ReflectionConstructibleType> GetReflectionConstructibleTypes (JniTypeSignature typeSignature)
-	{
-		foreach (var type in GetTypes (typeSignature)) {
-			yield return new ReflectionConstructibleType (type);
-		}
-	}
-
-	protected override Type? GetInvokerTypeCore ([DynamicallyAccessedMembers (Constructors)] Type type) => null;
-
-	protected override JniTypeSignature GetTypeSignatureCore (Type type)
-	{
-		var simpleReference = GetSimpleReference (type);
-		return simpleReference == null ? default : new JniTypeSignature (simpleReference, 0, false);
-	}
-
-	protected override IEnumerable<JniTypeSignature> GetTypeSignaturesCore (Type type)
-	{
-		var signature = GetTypeSignatureCore (type);
-		if (signature.IsValid)
-			yield return signature;
-	}
-
-	protected override IReadOnlyList<string>? GetStaticMethodFallbackTypesCore (string jniSimpleReference) => null;
-
-	protected override string? GetReplacementTypeCore (string jniSimpleReference) => null;
-
-	protected override JniRuntime.ReplacementMethodInfo? GetReplacementMethodInfoCore (string jniSimpleReference, string jniMethodName, string jniMethodSignature) => null;
 
 	public override void RegisterNativeMembers (
 			JniType nativeClass,
